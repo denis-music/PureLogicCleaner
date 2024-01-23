ac14c15273755a446a232c0dd7823dffeabdf069
ajdin.catic <ajdin.catic@edu.fit.ba>
2024-01-23 15:03:09 +0100

    integrate AI model as Azure function

---
+++ AI_Model/PredictionFunction/__init__.py
@@ -0,0 +1,16 @@
+import datetime
+import logging
+
+import azure.functions as func
+# from AI_Model.main import run_prediction
+
+
+def main(mytimer: func.TimerRequest) -> None:
+    utc_timestamp = datetime.datetime.utcnow().replace(
+        tzinfo=datetime.timezone.utc).isoformat()
+
+    if mytimer.past_due:
+        logging.info('The timer is past due!')
+
+    # run_prediction()
+    logging.info('Python timer trigger function ran at %s', utc_timestamp)
