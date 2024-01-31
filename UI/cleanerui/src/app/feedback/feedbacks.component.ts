8294f478add9d5d1b18fd72bc3c9df667b733a6a
NudzejmaK <157026935+NudzejmaK@users.noreply.github.com>
2024-01-31 23:49:19 +0100

    fix feedback form

--- UI/cleanerui/src/app/feedback/feedbacks.component.ts
+++ UI/cleanerui/src/app/feedback/feedbacks.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit, Type } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { FeedbacksService } from './feedbacks.service';
-import { Feedback } from '../model/feedback.model';
-import { FeedbackType } from '../enum/feedbackType.enum';
 import { StatisticsService } from '../statistics/statistics.service';
-import { RoomDate } from '../model/roomDate.model';
-import { Router } from '@angular/router';
 import { RoomService } from '../_services/room.service';
 import { UserRoomsService } from '../_services/user-rooms.service';
+import { RoomDate } from '../model/roomDate.model';
+import { Feedback } from '../model/feedback.model';
+import { FeedbackType } from '../enum/feedbackType.enum';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-feedbacks',
@@ -14,6 +15,9 @@ import { UserRoomsService } from '../_services/user-rooms.service';
   styleUrls: ['./feedbacks.component.scss']
 })
 export class FeedbacksComponent implements OnInit {
+  @ViewChild('feedbackForm', { static: false }) feedbackForm: NgForm | undefined;
+
+  optionList: RoomDate[] = [];
   cleaningHistoryId: string = '';
   text: string = '';
   ratingError: string = '';
@@ -26,32 +30,42 @@ export class FeedbacksComponent implements OnInit {
   rating: number = 0;
   feedbackText: string = '';
   showForm: boolean = true;
+  isSubmitting: boolean = false;
+  feedbackSubmitted: boolean = false;
 
-  constructor(private feedbackService: FeedbacksService,
-    private statisticsService: StatisticsService, private router: Router,
-    private roomService: RoomService, private userRoomService: UserRoomsService) {
+  constructor(
+    private feedbackService: FeedbacksService,
+    private statisticsService: StatisticsService,
+    private router: Router,
+    private roomService: RoomService,
+    private userRoomService: UserRoomsService
+  ) {
     this.feedbackTypeOptions = Object.entries(this.feedbackType)
       .filter(([key, value]) => !isNaN(Number(value)))
       .map(([key, value]) => ({ key, value: Number(value) }));
   }
 
-  optionList: RoomDate[] = [];
   ngOnInit(): void {
+    this.loadOptionList();
+  }
+
+  loadOptionList(): void {
     this.statisticsService.getCleaningsForMemberByMutalRoomId().subscribe(
       (data) => {
         if (data !== null) {
           data.forEach((item) => {
-            if (item)
+            if (item) {
               this.userRoomService.getUserRoomById(item.userRoomId).subscribe(
                 (userRoom) => {
                   this.roomService.getRoomById(userRoom!.roomId!).subscribe(
                     (room) => {
                       if (room !== null) {
-                        this.optionList.push(new RoomDate(item.userRoomId, room.customName, item.id, item.date))
+                        this.optionList.push(new RoomDate(item.userRoomId, room.customName, item.id, item.date));
                       }
-                    })
-                })
-          })
+                    });
+                });
+            }
+          });
         } else {
           console.error('There is no data.');
         }
@@ -67,9 +81,15 @@ export class FeedbacksComponent implements OnInit {
     const feedback = new Feedback(this.selectedCleaningType, feedbackTypeNumber,
       this.rating, this.feedbackText);
 
+    this.isSubmitting = true;
     this.feedbackService.saveFeedback(feedback).subscribe(
       response => {
         this.showForm = false;
+        if (this.feedbackForm) {
+          this.feedbackForm.resetForm();
+        }
+        this.isSubmitting = false;
+        this.feedbackSubmitted = true;
       },
       error => {
         console.error('Error submitting feedback:', error);
@@ -79,5 +99,22 @@ export class FeedbacksComponent implements OnInit {
 
   showPopup() {
     this.showForm = true;
+  }
+
+  closeForm() {
+    this.showForm = false;
+    if (this.feedbackForm) {
+      this.feedbackForm.resetForm();
+    }
+    this.router.navigate(['/home']);
+  }
+
+  submitAnotherFeedback() {
+    if (this.feedbackForm) {
+      this.feedbackForm.resetForm();
+    }
+    this.isSubmitting = false;
+    this.showForm = true;
+    this.feedbackSubmitted = false;
   }
 }
