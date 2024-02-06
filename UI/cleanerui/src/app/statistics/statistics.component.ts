9720d26e922ab66ed850a0005af5dba2f696822c
NudzejmaK <157026935+NudzejmaK@users.noreply.github.com>
2024-02-06 17:23:34 +0100

    chart ui

--- UI/cleanerui/src/app/statistics/statistics.component.ts
+++ UI/cleanerui/src/app/statistics/statistics.component.ts
@@ -1,16 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { StatisticsService } from './statistics.service';
 import { UserRoomsService } from '../_services/user-rooms.service';
 import { RoomService } from '../_services/room.service';
 import { StatsView } from '../model/stats.view.model';
 import { CleaningHistoryWithName } from '../model/cleaningHistoryWithName.model';
+import * as Highcharts from 'highcharts';
+import HighchartsMore from 'highcharts/highcharts-more';
+import HighchartsSolidGauge from 'highcharts/modules/solid-gauge';
+
+HighchartsMore(Highcharts);
+HighchartsSolidGauge(Highcharts);
 
 @Component({
   selector: 'app-statistics',
   templateUrl: './statistics.component.html',
   styleUrls: ['./statistics.component.scss']
 })
-export class StatisticsComponent implements OnInit {
+
+export class StatisticsComponent implements AfterViewInit {
 
   public doughnutData: Object[] = [
     { x: 'Completed', y: 60 },
@@ -31,8 +38,104 @@ export class StatisticsComponent implements OnInit {
   constructor(private statisticsService: StatisticsService,
     private userRoomsService: UserRoomsService, private roomService: RoomService) { }
 
-  ngOnInit() {
+  ngAfterViewInit() {
     this.loadData();
+    this.createChartGauge();
+  }
+
+  private getCompletedRoomsPercentage(): number {
+    //return Math.floor(Math.random() * (max - min + 1) + min)
+
+    this.isDataNeverLoaded = true;
+    this.apiResults = [];
+    if (this.selectedCleaningType) {
+      this.statisticsService.getCleaningsForMemberByMutalRoomId().subscribe(
+        (results) => {
+          if (results !== null) {
+            results.forEach((item) => {
+              if (item.userRoomId == this.selectedCleaningType && item.completed == true) {
+                var roomName = this.optionList.find(p => p.userRoomId == item.userRoomId)
+                var model = new CleaningHistoryWithName(
+                  item.id,
+                  item.userRoomId,
+                  roomName!.roomName, item.completed, "item.cleaningQuality"
+                  , item.cleaningDurationInMins, item.date,
+                  item.createdAt, item.updatedAt
+                )
+                this.apiResults.push(model);
+                this.isDataNeverLoaded = false;
+              }
+            })
+          }
+        }
+      );
+    }
+    return this.apiResults.length;   
+  }
+
+  private createChartGauge(): void {
+    const chart = Highcharts.chart('chart-gauge', 
+    {
+      chart: {
+        type: 'solidgauge',
+      },
+      title: {
+        text: 'Gauge Chart',
+      },
+      credits: {
+        enabled: false,
+      },
+      pane: {
+        startAngle: -90,
+        endAngle: 90,
+        center: ['50%', '85%'],
+        size: '160%',
+        background: {
+            innerRadius: '60%',
+            outerRadius: '100%',
+            shape: 'arc',
+        },
+      },
+      yAxis: {
+        min: 0,
+        max: 100,
+        stops: [
+          [0.1, '#DF5353'], // green
+          [0.5, '#DDDF0D'], // yellow
+          [0.9, '#55BF3B'], // red 
+        ],
+        minorTickInterval: null,
+        tickAmount: 2,
+        labels: {
+          y: 16,
+        },
+      },
+      plotOptions: {
+        solidgauge: {
+          dataLabels: {
+            y: -25,
+            borderWidth: 0,
+            useHTML: true,
+          },
+        },
+      },
+      tooltip: {
+        enabled: false,
+      },
+      series: [{
+        name: null,
+        data: [this.getCompletedRoomsPercentage()],
+        dataLabels: {
+          format: '<div style="text-align: center"><span style="font-size: 1.25rem">{y}</span></div>',
+        },
+      }],
+    } as any);
+
+    console.log("here")
+
+    // setInterval(() => {
+    //   chart.series[0].points[0].update(this.getRandomNumber(0, 100));
+    // }, 1000);
   }
 
   private loadData() {
